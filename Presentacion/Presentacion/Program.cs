7cb09b942a0e537d96e09fdc9fdb6490a2314d8a
Miguel Angel Jimenez Melendez <miguejm@outlook.es>
2020-08-04 16:02:13 -0500

    primer programa

---
+++ Presentacion/Presentacion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Presentacion
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //comentario de linea
+            /*
+                Escrito por MiGueAJM9724
+                Fecha de creación: 4/Ago/2020
+             */
+            Console.WriteLine("Hello World!"); //salto de linea
+            Console.Write("Hello word!");
+            Console.ReadKey();
+        }
+    }
+}
